6e30bf345583c71fdc9e4cc9b23fd242b6273a8d
manikann <manikann@gmail.com>
2018-08-21 21:42:33 +0800

    housekeeping

--- connect.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-PID=$(ps -ef | grep '[k]ubectl' | tr -s ' ' '|' | cut -f3 -d '|') 
-if [[ ! -z "$PID" ]]; then
-  kill $PID
-fi
-kubectl -n=spinnaker port-forward $(kubectl -n=spinnaker get pods -l=load-balancer-spin-deck=true -o=jsonpath='{.items[0].metadata.name}') 9000 > /dev/null &
-kubectl -n=spinnaker port-forward $(kubectl -n=spinnaker get pods -l=load-balancer-spin-gate=true -o=jsonpath='{.items[0].metadata.name}') 8084 > /dev/null &
